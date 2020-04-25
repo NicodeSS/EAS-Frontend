d49ec88bd03a4f9376b7a790794bb2dea0cf3172
NicodeSS <RD_Nicode@outlook.com>
2020-04-25 20:04:10 +0800

    feat: dynamic adjust sidebar content

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,99 @@
 <template>
-  <v-app>
+  <v-app id="inspire">
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+      clipped
+    >
+      <v-list dense>
+        <template v-for="item in items">
+          <v-list-group v-if="item.children" :prepend-icon="item.icon">
+            <template v-slot:activator>
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </template>
+              <v-list-item link v-for="child in item.children" :key="index">
+                <v-list-item-action>
+                  <v-icon>{{ child.icon }}</v-icon>
+                </v-list-item-action>
+                <v-list-item-content>
+                  <v-list-item-title>{{ child.title }}</v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+          </v-list-group>
+          <v-list-item link v-else>
+            <v-list-item-action>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </template>
+      </v-list>
+    </v-navigation-drawer>
+
     <v-app-bar
       app
-      color="primary"
-      dark
+      clipped-left
     >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-toolbar-title>{{Title}}</v-toolbar-title>
     </v-app-bar>
 
     <v-content>
-      <HelloWorld/>
+      <router-view></router-view>
     </v-content>
+
+    <v-footer app>
+      <span>Nicode &copy; 2020</span>
+    </v-footer>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+  export default {
+    props: {
+      source: String,
+    },
+    data: () => ({
+      drawer: null,
+      Title: "EAS",
+      items: [
+        {
+          title: "员工管理",
+          icon: "mdi-account",
+        },
+        {
+          title: "排班管理",
+          icon: "mdi-account-clock",
+        },
+        {
+          title: "排班展示",
+          icon: "mdi-calendar-month-outline",
+        },
+        {
+          title: "活动审批",
+          icon: "mdi-check-underline"
+          // children: [
+          //   {
+          //     title: "x1",
+          //     icon: "mdi-settings",
+          //   },
+          //   {
+          //     title: "x2",
+          //     icon: "mdi-settings",
+          //   }
+          // ]
+        },
 
-export default {
-  name: 'App',
-
-  components: {
-    HelloWorld,
-  },
-
-  data: () => ({
-    //
-  }),
-};
+        {
+          title: "考勤管理",
+          icon: "mdi-check-outline",
+        }
+      ]
+    }),
+    created () {
+      this.$vuetify.theme.dark = true;
+    }
+  }
 </script>
