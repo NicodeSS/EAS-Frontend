489e835b129b9cde8bc153e9d2a03970dfcf1512
NicodeSS <RD_Nicode@outlook.com>
2020-05-30 23:32:43 +0800

    fix: add missing store action in !5

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,3 +1,5 @@
+import Vue from "vue";
+
 export const setUser = ({ commit, store }, user) => {
   if (user) {
     commit("changeUserStatus", true);
@@ -14,4 +16,28 @@ export const setUser = ({ commit, store }, user) => {
     commit("changeUserToken", null);
     localStorage.removeItem("token");
   }
+};
+
+export const validateUser = async ({ dispatch }) => {
+  if (localStorage.getItem("token")) {
+    try {
+      let result = await Vue.prototype.$http.get("/manager/authorization.do");
+      let content = result.data.data;
+      let user = {
+        id: content.uid,
+        name: content.uName,
+        role: content.role,
+        token: content.token
+      };
+      console.log(content);
+      localStorage.setItem("token", user.token);
+      await dispatch("setUser", user);
+      return true;
+    } catch (err) {
+      console.log(err);
+      localStorage.removeItem("token");
+      await dispatch("setUser", null);
+      return false;
+    }
+  } else return false;
 };
