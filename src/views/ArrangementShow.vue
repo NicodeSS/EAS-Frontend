ba0fd458a51125cacb7682247d3dd55b6ab1e840
NicodeSS <RD_Nicode@outlook.com>
2020-06-09 00:38:02 +0800

    feat(Arrangement Show): initialize page structure

---
+++ src/views/ArrangementShow.vue
@@ -0,0 +1,18 @@
+<template>
+  <employee-arrangement></employee-arrangement>
+</template>
+
+<script>
+import EmployeeArrangement from "../components/EmployeeArrangement";
+export default {
+  name: "ArrangementShow",
+  components: { EmployeeArrangement },
+  data() {
+    return {
+      uId: 1
+    };
+  }
+};
+</script>
+
+<style scoped></style>
