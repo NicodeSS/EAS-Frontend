c7c276ce0dfdbf0719d632932fd1914873b5f64c
NicodeSS <RD_Nicode@outlook.com>
2020-06-10 23:24:15 +0800

    chore: add warning interceptor

--- src/main.js
+++ src/main.js
@@ -10,6 +10,18 @@ import axiosApi from "./plugins/axios";
 Vue.config.productionTip = false;
 Vue.prototype.$http = axiosApi;
 
+// Warning Interceptor
+const ignoreWarnMessage =
+  "The .native modifier for v-on is only valid on components but it was used on <div>.";
+Vue.config.warnHandler = function(msg, vm, trace) {
+  // `trace` is the component hierarchy trace
+  if (msg === ignoreWarnMessage) {
+    msg = null;
+    vm = null;
+    trace = null;
+  }
+};
+
 /* eslint-disable no-new */
 new Vue({
   el: "#app",
